5de9942cc513fdc4ae7dd699f684f5a5b23dce7c
davidomahony <daverock914@gmail.com>
2024-03-12 22:15:33 +0000

    Deletion

--- src/TokenizationService.API/TokenizationService.API/Repositories/TokenRepository.cs
+++ src/TokenizationService.API/TokenizationService.API/Repositories/TokenRepository.cs
@@ -3,39 +3,21 @@ using System.Data.SqlClient;
 
 namespace TokenizationService.API.Repositories
 {
-    public class TokenRepository : IRepository<TokenObject>
+    public abstract class TokenRepository : IRepository<TokenObject>
     {
-        private SqlConnection dbConnection;
+        protected IConfiguration configuration;
 
         // this class needs to be cleaned up its load of shite
 
         public TokenRepository(IConfiguration configuration)
         {
-            string connectionString = configuration["sqlconnection"] 
-                ?? throw new ArgumentNullException(nameof(configuration));
-            this.dbConnection = new SqlConnection(connectionString);
+            this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         }
 
-        public Task<TokenObject> CreateAsync(TokenObject entity)
-        {
-            var command = this.dbConnection.CreateCommand();
-            command.CommandText = $@"
-                NSERT INTO example (token, stringvalue) VALUES
-                ('*-{entity.Token}-*', '{entity.Value}')";
+        public abstract Task<TokenObject> CreateAsync(TokenObject entity);
 
-            var result = command.BeginExecuteNonQuery();
+        public abstract Task<TokenObject> ReadAsync(string id);
 
-            return null;
-        }
-
-        public Task<TokenObject> ReadAsync(string id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<TokenObject> UpdateAsync(string id, TokenObject entity)
-        {
-            throw new NotImplementedException();
-        }
+        public abstract Task<TokenObject> UpdateAsync(string id, TokenObject entity);
     }
 }
